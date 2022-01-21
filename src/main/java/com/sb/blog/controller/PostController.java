9f3daf72dd1eb66f94023ca39353822a90e96f43
Animesh Kumar <animeshy2jj@gmail.com>
2022-01-22 01:35:02 +0530

    Api Security. User and Roles config. Authentication through Database. Securing APIs.

--- src/main/java/com/sb/blog/controller/PostController.java
+++ src/main/java/com/sb/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.sb.blog.service.PostService;
 import com.sb.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -21,6 +22,7 @@ public class PostController {
     }
 
     //create blog post
+    @PreAuthorize(("hasRole('ADMIN')"))
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
@@ -38,11 +40,13 @@ public class PostController {
         return ResponseEntity.ok(postService.getpostById(id));
     }
 
+    @PreAuthorize(("hasRole('ADMIN')"))
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") Long id) {
         return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
+    @PreAuthorize(("hasRole('ADMIN')"))
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable Long id) {
         return new ResponseEntity<>((postService.deletePost(id) ? "Post deleted Successfully." : "Post deletion failed."), HttpStatus.OK);
---
+++ src/main/java/com/sb/blog/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.sb.blog.repository;
+
+import com.sb.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
